f9b6fd1596ed9ddcdd65e9b63981a4735db1f30d
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-10-14 16:41:49 -0300

    :sparkles: feat: Add auth pages
    Add sign in and sign up pages

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Image } from 'react-native';
+import PropTypes from 'prop-types';
+
+import logo from '~/assets/logo.png';
+
+import Background from '~/components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Full name"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Type your e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Your secret password"
+          />
+
+          <SubmitButton on={() => {}}>Access</SubmitButton>
+
+          <SignLink
+            onPress={() => {
+              navigation.navigate('SignIn');
+            }}
+          >
+            <SignLinkText>Already have an account</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
+    </Background>
+  );
+}
+
+SignUp.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
