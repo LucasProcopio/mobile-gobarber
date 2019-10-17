394db7afaebf71a4f9c61278b88a7236d315870b
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-10-17 15:29:42 -0300

    :rocket: feat: Add user provider appointment

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,63 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { formatRelative, parseISO } from 'date-fns';
 
-// import { Container } from './styles';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Confirm() {
-  return <View />;
+import Background from '~/components/Background';
+import { Provider } from '../SelectProvider/styles';
+
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+import api from '~/services/api';
+
+export default function Confirm({ navigation }) {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date()),
+    [time]
+  );
+
+  async function handleAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: Provider.Avatar
+              ? Provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButton onPress={() => handleAppointment()}>Confirm</SubmitButton>
+      </Container>
+    </Background>
+  );
 }
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirm data',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
